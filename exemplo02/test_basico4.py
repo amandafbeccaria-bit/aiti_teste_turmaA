140414743d5efeee74b24532724193d9dad89f27
Amanda França <amandafbecca@outlook.com>
2025-11-03 11:38:10 -0300

    pasta exemplo02

---
+++ exemplo02/test_basico4.py
@@ -0,0 +1,7 @@
+from funcoes import *
+def test_dividir():
+   assert dividir(4,2) == 2
+   assert dividir(4,0) is None
+def test_email_valido():
+   assert email_valido("amanda@.") is True
+   assert email_valido("exemplo.com") is False 
